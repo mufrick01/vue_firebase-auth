882cd3f0d331dc42caada78fb0d55ffa1be349f6
mufrick01 <mrmufrick@gmail.com>
2024-11-15 20:36:14 -0300

    forms validation

--- src/modules/auth/pages/LoginPage.vue
+++ src/modules/auth/pages/LoginPage.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="h-100 d-flex flex-column justify-content-center mt-5 mt-lg-0">
-    <h2>LoginPage works</h2>
+    <h2 class="text-center">Login</h2>
+
     <form @submit.prevent="login">
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <label for="exampleInputEmail1" class="form-label" :class="emailStyleStatus">Email address</label>
+        <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+          required @input="emailChange">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
       </div>
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input v-model="password" type="password" class="form-control" id="exampleInputPassword1">
+        <label for="exampleInputPassword1" class="form-label" :class="passwordStyleStatus">Password</label>
+        <input v-model="password" @input="passwordChange" type="password" class="form-control"
+          id="exampleInputPassword1" required>
       </div>
       <!-- <div class="mb-3 form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -29,24 +32,59 @@ import { useRouter } from 'vue-router';
 const email = ref('');
 const password = ref('');
 
+const emailStyleStatus = ref('');
+const passwordStyleStatus = ref('');
+
 const router = useRouter();
 
 const login = async () => {
 
   try {
+
+    if (email.value.trim() === '' || password.value.trim() === '' || password.value.trim().length < 6) {
+      throw Error('invalid fields')
+    }
     const { user } = await signInWithEmailAndPassword(auth, email.value, password.value);
-    console.log('logeado con éxito')
 
-
-    console.log(user.accessToken)
     localStorage.setItem('userToken', user.accessToken)
 
     router.replace({ name: 'home' })
   } catch (error) {
-    alert('Error en login', error.message)
+    alert('invalid fields', error.message)
   }
+}
+
+
+const emailChange = () => {
+
+  console.log('email change')
+
+  if (email.value.trim() === '') {
+    return emailStyleStatus.value = ''
+  }
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (email.value.trim().match(regex)) {
+    return emailStyleStatus.value = 'text-success'
+  }
+
+  return emailStyleStatus.value = 'text-danger'
+}
+
+const passwordChange = () => {
+
+
+  if (password.value.trim() === '') {
+    return passwordStyleStatus.value = ''
+  }
+  if (password.value.trim().length >= 6) {
+    return passwordStyleStatus.value = 'text-success'
+  }
+
+  return passwordStyleStatus.value = 'text-danger'
 }
 
 
 
 </script>
+
+<style scoped></style>
--- src/modules/auth/pages/RegisterPage.vue
+++ src/modules/auth/pages/RegisterPage.vue
@@ -1,20 +1,22 @@
 <template>
   <div class=" h-100 d-flex flex-column justify-content-center mt-5 mt-lg-0">
-    <h2>RegisterPage works</h2>
+    <h2 class="text-center">Register</h2>
     <form @submit.prevent="register">
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <label for="exampleInputEmail1" class="form-label" :class="emailStyleStatus">Email address</label>
+        <input v-model="email" @input="emailChange" type="email" class="form-control" id="exampleInputEmail1"
+          aria-describedby="emailHelp">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
       </div>
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input v-model="password" type="password" class="form-control" id="exampleInputPassword1">
+        <label for="exampleInputPassword1" class="form-label" :class="passwordStyleStatus">Password</label>
+        <input v-model="password" @input="passwordChange" type="password" class="form-control"
+          id="exampleInputPassword1">
       </div>
-      <div class="mb-3 form-check">
+      <!-- <div class="mb-3 form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
         <label class="form-check-label" for="exampleCheck1">I accept the <a href="#">terms and conditions</a></label>
-      </div>
+      </div> -->
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
     <RouterLink :to="{ name: 'login' }">have account? login here</RouterLink>
@@ -28,12 +30,20 @@ import { useRouter } from 'vue-router';
 
 const email = ref('');
 const password = ref('');
+
+const emailStyleStatus = ref('');
+const passwordStyleStatus = ref('');
+
 const router = useRouter();
 
 
 const register = async () => {
 
   try {
+    if (email.value.trim() === '' || password.value.trim() === '' || password.value.trim().length < 6) {
+      throw Error('invalid fields')
+    }
+
     const userCredentials = await createUserWithEmailAndPassword(auth, email.value, password.value);
     const user = userCredentials.user;
     console.log(user);
@@ -41,8 +51,38 @@ const register = async () => {
 
     router.replace({ name: 'home' });
   } catch (error) {
-    alert('error al registrar usuario', error.message);
+    alert('invalid fields', error.message);
   }
 
 }
+
+
+const emailChange = () => {
+
+  if (email.value.trim() === '') {
+    return emailStyleStatus.value = ''
+  }
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (email.value.trim().match(regex)) {
+    return emailStyleStatus.value = 'text-success'
+  }
+
+  return emailStyleStatus.value = 'text-danger'
+}
+
+const passwordChange = () => {
+
+
+  if (password.value.trim() === '') {
+    return passwordStyleStatus.value = ''
+  }
+  if (password.value.trim().length >= 6) {
+    return passwordStyleStatus.value = 'text-success'
+  }
+
+  return passwordStyleStatus.value = 'text-danger'
+}
+
+
+
 </script>
